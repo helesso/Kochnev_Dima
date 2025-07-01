eac1ac6ab193e20e0b73599f47df293e839f1826
helesso <dimkoch04@gmail.com>
2025-07-01 11:03:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
+
 #define LED_PIN 2
-void setup() {
+
+void setup(){
   pinMode(LED_PIN,OUTPUT);
   Serial.begin(115200);
-  Serial.println("ESP32")
+  Serial.println('ESP32')
+}
+
+void loop(){
+  digitalWrite(LED_PIN,HIGH);
+  Serial.println('LED ON');
+  delay(100);
+
+  digitalWrite(LED_PIN,LOW);
+  Serial.println('LED OFF');
+  delay(3000);
 }
